c195f0e72a1d1414146651ddadbe5d2bed7b8a86
annakrasner <krasner.anna@gmail.com>
2019-03-20 10:21:45 -0700

    Create ssb-interop-js-addon.js

---
+++ ssb-interop-js-addon.js
@@ -0,0 +1,45 @@
+
+//dark theme
+// First make sure the wrapper app is loaded
+document.addEventListener("DOMContentLoaded", function() {
+
+   // Then get its webviews
+   let webviews = document.querySelectorAll(".TeamView webview");
+
+   // Fetch our CSS in parallel ahead of time
+   const cssPath = 'https://raw.githubusercontent.com/annakrasner/slack-dark-theme-css/master/slack-styles-dark-comfy.css';
+   let cssPromise = fetch(cssPath).then(response => response.text());
+
+   let customCustomCSS = `
+
+
+
+
+   `
+
+   // Insert a style tag into the wrapper view
+   cssPromise.then(css => {
+      let s = document.createElement('style');
+      s.type = 'text/css';
+      s.innerHTML = css + customCustomCSS;
+      document.head.appendChild(s);
+   });
+
+   // Wait for each webview to load
+   webviews.forEach(webview => {
+      webview.addEventListener('ipc-message', message => {
+         if (message.channel == 'didFinishLoading')
+            // Finally add the CSS into the webview
+            cssPromise.then(css => {
+               let script = `
+                     let s = document.createElement('style');
+                     s.type = 'text/css';
+                     s.id = 'slack-custom-css';
+                     s.innerHTML = \`${css + customCustomCSS}\`;
+                     document.head.appendChild(s);
+                     `
+               webview.executeJavaScript(script);
+            })
+      });
+   });
+})
